6f5ad464e15ded7e46fd31c7644357758919c7b7
elvight <43282984+elvight@users.noreply.github.com>
2018-09-14 17:43:22 -0500

    Update parte1.m

--- parte1.m
+++ parte1.m
@@ -1,9 +1,14 @@
+
+%modelo de regresion lineal para comprar una vivienda con datos de entrenamiento
+%PRESENTADO POR: QUISPE CUEVAS ELVIS GUIDO
+
+%PRIMERO TENEMOS QUE CARGAR LOS DATOS DE ENTRENAMIENT
 data = load ('data2.txt');
 X=data(:,1);
 Y=data(:,2);
 Z=data(:,3);
 m=length(Z);
-
+ %PARA HACER MUESTRA DE LA IMAGEN EN 3D
 figure;
  hold on;
  plot3(X,Y,Z,'rx',' Markersize',10);
